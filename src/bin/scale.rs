647706ac7f405e16ddb7d5cce6d638fff36080b2
Manos Pitsidianakis <el13635@mail.ntua.gr>
2021-11-21 20:08:33 +0200

    Add code for bitmap scaling

---
+++ src/bin/scale.rs
@@ -0,0 +1,73 @@
+use bitmappers_geometry::*;
+use minifb::{Key, Window, WindowOptions};
+
+pub fn distance_between_two_points(p_k: Point, p_l: Point) -> f64 {
+    let (x_k, y_k) = p_k;
+    let (x_l, y_l) = p_l;
+    let xlk = x_l - x_k;
+    let ylk = y_l - y_k;
+    f64::sqrt((xlk * xlk + ylk * ylk) as f64)
+}
+
+include!("../dmr.xbm.rs");
+const WINDOW_WIDTH: usize = 400;
+const WINDOW_HEIGHT: usize = 400;
+
+fn main() {
+    let mut buffer: Vec<u32> = vec![WHITE; WINDOW_WIDTH * WINDOW_HEIGHT];
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        WindowOptions {
+            title: true,
+            //borderless: true,
+            //resize: false,
+            //transparency: true,
+            ..WindowOptions::default()
+        },
+    )
+    .unwrap();
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    let mut original = Image::new(DMR_WIDTH, DMR_HEIGHT, 25, 25);
+    original.bytes = bits_to_bytes(DMR_BITS, DMR_WIDTH);
+    original.draw(&mut buffer, BLACK, None, WINDOW_WIDTH);
+
+    let mut scaled = Image::new(DMR_WIDTH * 5, DMR_HEIGHT * 5, 100, 100);
+    let mut sx: i64; //source
+    let mut sy: i64; //source
+    let mut dx: i64; //destination
+    let mut dy: i64 = 0; //destination
+
+    let og_height = original.height as i64;
+    let og_width = original.width as i64;
+    let scaled_height = scaled.height as i64;
+    let scaled_width = scaled.width as i64;
+
+    while dy < scaled_height {
+        sy = (dy * og_height) / scaled_height;
+        dx = 0;
+        while dx < scaled_width {
+            sx = (dx * og_width) / scaled_width;
+            if original.get(sx, sy) == Some(BLACK) {
+                scaled.plot(dx, dy);
+            }
+            dx += 1;
+        }
+        dy += 1;
+    }
+    scaled.draw(&mut buffer, BLACK, None, WINDOW_WIDTH);
+
+    while window.is_open() && !window.is_key_down(Key::Escape) && !window.is_key_down(Key::Q) {
+        window
+            .update_with_buffer(&buffer, WINDOW_WIDTH, WINDOW_HEIGHT)
+            .unwrap();
+
+        let millis = std::time::Duration::from_millis(100);
+
+        std::thread::sleep(millis);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 pub type Point = (i64, i64);
 
 pub const fn from_u8_rgb(r: u8, g: u8, b: u8) -> u32 {
@@ -66,12 +67,12 @@ impl Image {
         self.bytes[y * self.width + x] = BLACK;
     }
 
-    pub fn get(&mut self, x: i64, y: i64) -> u32 {
-        if x < 0 || y < 0 {
-            return WHITE;
+    pub fn get(&mut self, x: i64, y: i64) -> Option<u32> {
+        if x < 0 || y < 0 || y >= (self.height as i64) || x >= (self.width as i64) {
+            return None;
         }
         let (x, y): (usize, usize) = (x as _, y as _);
-        self.bytes[y * self.width + x]
+        Some(self.bytes[y * self.width + x])
     }
 
     pub fn plot_ellipse(
@@ -242,40 +243,64 @@ impl Image {
     }
 
     pub fn flood_fill(&mut self, mut x: i64, y: i64) {
-        eprintln!("flood fill x, y {:?}", (x, y));
-        if self.get(x, y) != WHITE {
+        if self.get(x, y) != Some(WHITE) {
             return;
         }
-        let mut s = Vec::new();
-        s.push((x, x, y + 1, 1));
-        s.push((x, x, y, -1));
-        while let Some((mut x1, x2, y, dy)) = s.pop() {
-            x = x1;
-            if self.get(x, y) == WHITE {
-                //Inside(x, y):
-                while self.get(x - 1, y) == WHITE {
-                    // Inside(x - 1, y):
-                    self.plot(x - 1, y);
-                    x = x - 1;
-                }
+
+        let w = (self.width as i64);
+        let h = (self.height as i64);
+        let mut span_above: bool;
+        let mut span_below: bool;
+
+        let mut s = VecDeque::new();
+        s.push_back((x, y));
+        while let Some((x, y)) = s.pop_back() {
+            let mut x1 = x;
+            while x1 >= 0 && self.get(x1, y).map(|some| some == WHITE).unwrap_or(false) {
+                x1 -= 1;
             }
-            if x < x1 {
-                s.push((x, x1 - 1, y - dy, -dy));
-            }
-            while x1 < x2 {
-                while self.get(x1, y) == WHITE {
-                    //Inside(x1, y):
-                    self.plot(x1, y);
-                    x1 = x1 + 1;
-                    s.push((x, x1 - 1, y + dy, dy));
-                    if x1 - 1 > x2 {
-                        s.push((x2 + 1, x1 - 1, y - dy, -dy));
-                    }
-                    while x1 < x2 && self.get(x1, y) != WHITE {
-                        x1 = x1 + 1;
-                    }
-                    x = x1;
+            x1 += 1;
+            span_above = false;
+            span_below = false;
+            while x1 < w && self.get(x1, y).map(|some| some == WHITE).unwrap_or(false) {
+                self.plot(x1, y);
+                if !span_above
+                    && y > 0
+                    && self
+                        .get(x1, y - 1)
+                        .map(|some| some == WHITE)
+                        .unwrap_or(false)
+                {
+                    s.push_back((x1, y - 1));
+                    span_above = true;
+                } else if span_above
+                    && y > 0
+                    && self
+                        .get(x1, y - 1)
+                        .map(|some| some != WHITE)
+                        .unwrap_or(false)
+                {
+                    span_above = false;
                 }
+                if !span_below
+                    && y < h - 1
+                    && self
+                        .get(x1, y + 1)
+                        .map(|some| some == WHITE)
+                        .unwrap_or(false)
+                {
+                    s.push_back((x1, y + 1));
+                    span_below = true;
+                } else if span_below
+                    && y < h - 1
+                    && self
+                        .get(x1, y + 1)
+                        .map(|some| some != WHITE)
+                        .unwrap_or(false)
+                {
+                    span_below = false;
+                }
+                x1 += 1;
             }
         }
     }
