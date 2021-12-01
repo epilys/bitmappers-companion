d1672f4866e02dbec63d892900fa42b4e53acb57
Manos Pitsidianakis <el13635@mail.ntua.gr>
2021-12-01 16:26:43 +0200

    intro: add rust tool to convert xbm to rust code

---
+++ src/bin/xbmtors.rs
@@ -0,0 +1,39 @@
+use regex;
+use regex::Regex;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let args = std::env::args().skip(1).collect::<Vec<String>>();
+    if args.len() != 1 {
+        println!("one argument expected, the xbm file path to convert.");
+        return;
+    }
+    let mut file = match File::open(&args[0]) {
+        Err(err) => panic!("couldn't open {}: {}", args[0], err),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    if let Err(err) = file.read_to_string(&mut s) {
+        panic!("couldn't read {}: {}", args[0], err);
+    }
+
+    let re = Regex::new(
+        r"(?imx)
+  ^\s*\x23\s*define\s+(?P<i>.+?)_width\s+(?P<w>\d\d*)$
+  \s*
+  ^\s*\x23\s*define\s+.+?_height\s+(?P<h>\d\d*)$
+  \s*
+  ^\s*static(\s+unsigned){0,1}\s+char\s+.+?_bits..\s*=\s*\{(?P<b>[^}]+)\};
+",
+    )
+    .unwrap();
+
+    let caps = re
+        .captures(&s)
+        .expect("Could not convert file, regex doesn't match :(");
+    let ident = caps.name("i").unwrap().as_str().to_uppercase();
+    let out = re.replace_all(&s, format!("const {i}_WIDTH: usize = $w;\nconst {i}_HEIGHT: usize = $h;\nconst {i}_BITS: &[u8] = &[$b];", i = &ident));
+    println!("{}", out.trim());
+}
