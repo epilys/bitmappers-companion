06da7b067942b69f4c8f3c05cc37cfa9910959ed
Manos Pitsidianakis <el13635@mail.ntua.gr>
2021-12-01 21:21:22 +0200

    Add hilbert curve chapter

---
+++ src/bin/hilbert.rs
@@ -0,0 +1,109 @@
+use bitmappers_companion::*;
+use minifb::{Key, Window, WindowOptions};
+use std::f64::consts::FRAC_PI_2;
+
+const WINDOW_WIDTH: usize = 200;
+const WINDOW_HEIGHT: usize = 200;
+
+const HILBERT: &[&[usize]] = &[
+    &[22, 10, 16, 38],
+    &[10, 22, 24, 48],
+    &[44, 36, 30, 18],
+    &[36, 44, 42, 28],
+];
+
+fn curve(img: &mut Image, k: usize, order: i64, mut x: i64, mut y: i64) -> (i64, i64) {
+    const STEP_SIZE: i64 = 5;
+    let mut row: usize;
+    let mut direction: usize;
+    if order > 0 {
+        for j in 0..4 {
+            let step = HILBERT[k][j];
+            row = (step / 10) - 1;
+            let (xn, yn) = curve(img, row, order - 1, x, y);
+            x = xn;
+            y = yn;
+            direction = step % 10;
+            let prev = (x, y);
+            match direction {
+                8 => {
+                    // null op
+                }
+                2 => {
+                    //N
+                    y -= STEP_SIZE;
+                }
+                1 => {
+                    // NE
+                    y -= STEP_SIZE;
+                    x += STEP_SIZE;
+                }
+                0 => {
+                    //E
+                    x += STEP_SIZE;
+                }
+                7 => {
+                    //SE
+                    x += STEP_SIZE;
+                    y += STEP_SIZE;
+                }
+                6 => {
+                    //S
+                    y += STEP_SIZE;
+                }
+                5 => {
+                    //SW
+                    y += STEP_SIZE;
+                    x -= STEP_SIZE;
+                }
+                4 => {
+                    //W
+                    x -= STEP_SIZE;
+                }
+                3 => {
+                    //NW
+                    y -= STEP_SIZE;
+                    x -= STEP_SIZE;
+                }
+                other => unreachable!("{}", other),
+            }
+            img.plot_line_width(prev, (x, y), 0.);
+        }
+    }
+    (x, y)
+}
+
+fn main() {
+    let mut buffer: Vec<u32> = vec![WHITE; WINDOW_WIDTH * WINDOW_HEIGHT];
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        WindowOptions {
+            title: true,
+            //borderless: true,
+            resize: true,
+            //transparency: true,
+            ..WindowOptions::default()
+        },
+    )
+    .unwrap();
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    let mut image = Image::new(WINDOW_WIDTH, WINDOW_WIDTH, 0, 0);
+    curve(&mut image, 0, 10, 0, WINDOW_WIDTH as i64);
+
+    while window.is_open() && !window.is_key_down(Key::Escape) && !window.is_key_down(Key::Q) {
+        image.draw(&mut buffer, BLACK, None, WINDOW_WIDTH);
+
+        window
+            .update_with_buffer(&buffer, WINDOW_WIDTH, WINDOW_HEIGHT)
+            .unwrap();
+
+        let millis = std::time::Duration::from_millis(100);
+
+        std::thread::sleep(millis);
+    }
+}
